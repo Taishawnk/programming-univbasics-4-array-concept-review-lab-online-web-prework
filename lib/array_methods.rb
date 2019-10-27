ec4d3728a7fe7df2991bdbfb639b0a8fae6fa37a
Taishawn King <taishawnking1@yahoo.com>
2019-10-27 18:13:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  found_index = nil #initialize a found_index variable
+  found_index = 2 
   counter = 0
   while counter < array.size do
     counter += 1
